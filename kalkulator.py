06c57154b83b1570d3b0d57f851626700c6b003d
dembiak <124161497+dembiak@users.noreply.github.com>
2023-02-01 15:21:19 +0100

    Add files via upload

--- kalkulator.py
+++ kalkulator.py
@@ -1,8 +1,12 @@
 a = int(input("Podaj liczbe "))
 b = int(input("Podaj druga liczbe "))
-operacja = input("Podaj dzialanie matematyczne +,-")
+operacja = input("Podaj dzialanie matematyczne +,-,*,/")
 print(operacja)
 if (operacja == "+"):
     print("Wynik dodawania ",a+b)
 if (operacja == "-"):
     print("Wynik odejmowania", a-b)
+if (operacja == "*"):
+    print("Wynik mnozenia ", a*b)
+if (operacja == "/"):
+    print("Wynik dzielenia ", a/b)
