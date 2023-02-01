e1077e080860900939daf728d74685e7b566fba6
dembiak <minimaster532@gmail.com>
2023-02-01 14:31:03 +0100

    dodanie kalkulatora do folderu

---
+++ kalkulator.py
@@ -0,0 +1,8 @@
+a = int(input("Podaj liczbe "))
+b = int(input("Podaj druga liczbe "))
+operacja = input("Podaj dzialanie matematyczne +,-")
+print(operacja)
+if (operacja == "+"):
+    print("Wynik dodawania ",a+b)
+if (operacja == "-"):
+    print("Wynik odejmowania", a-b)
